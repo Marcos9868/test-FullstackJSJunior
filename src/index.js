e426fa767b9b3f342cc946acb23478f8fb12d3cf
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-03-23 14:09:35 -0300

    Adiciona a base da api e centraliza todos os componentes

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// Importações dos módulos necessários
+const express = require('express')
+const api = express()
+const userRoute = require('./routes/users.status')
+const status = require('./routes/status.route')
+
+// Configuração da api para receber JSON
+api.use(express.json())
+
+// Status da API
+api.use(status)
+
+// Rota dos usuários
+api.use(userRoute)
+
+// Porta do servidor
+api.listen(4000, () => {
+  console.log('Servidor rodando no link http://localhost:4000')
+})
