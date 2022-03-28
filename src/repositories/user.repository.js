3db3a41a0052783f98651365b25e4787eb3e5603
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-03-28 10:59:03 -0300

    Adiciona a funçao que recupera a informaçao de um usuario pelo seu ID

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -4,12 +4,27 @@ const User = require('../models/model.user')
 
 // Código que faz o acesso ao banco de dados
 class userRepository {
+  // Função que traz todos os usuários
   async findAllUsers() {
     const query = `
       SELECT uuid, useremail FROM list_users
     `;
     const { rows } = await db.query(query)
     return rows || []
+  }
+  // Função que traz apenas um usuário pelo seu ID único
+  async findById(uuid) {
+    const query = `
+      SELECT uuid, useremail 
+      FROM list_users
+      WHERE uuid = $1 
+    `
+    const values = [uuid]
+
+    const { rows } = await db.query(query, values)
+    const [ user ] = rows
+
+    return user || []
   }
 }
 
