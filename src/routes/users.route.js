3a1f810fc7f7015945c384d13cf10dd8c2f621d4
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-03-28 17:21:57 -0300

    Trata a requisiçao da consulta dos dados e caso ocorra um erro, este sera exibido de forma legivel

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -4,21 +4,26 @@ const userRepository = require('../repositories/user.repository')
 const usersRoute = router()
 
 // Lista todos os usuários
-usersRoute.get('/api/v1/users', async (req, res) => {
+usersRoute.get('/api/v1/users', async (req, res, next) => {
   const users = await userRepository.findAllUsers()
   res.send(users)
 })
 
 // Lista um usuário específico
-usersRoute.get('/api/v1/users/:uuid', async (req, res) => {
-  const uuid = req.params.uuid
-  const user = await userRepository.findById(uuid)
-  console.log(user)
-  res.send(user)
+usersRoute.get('/api/v1/users/:uuid', async (req, res, next) => {
+  try {
+    const uuid = req.params.uuid
+    const user = await userRepository.findById(uuid)
+    console.log(user)
+    res.send(user)
+
+  } catch(error) {
+    next(error)
+  }
 })
 
 // Cria um usuário
-usersRoute.post('/api/v1/users', async (req, res) => {
+usersRoute.post('/api/v1/users', async (req, res, next) => {
   const newUser = req.body
   const uuid = await userRepository.createUser(newUser)
   res.status(201).send(uuid)
@@ -26,7 +31,7 @@ usersRoute.post('/api/v1/users', async (req, res) => {
 })
 
 // Altera dados de um usuário
-usersRoute.put('/api/v1/users/:uuid', async (req, res) => {
+usersRoute.put('/api/v1/users/:uuid', async (req, res, next) => {
   const uuid = req.params.uuid
   const modifiedUser = req.body
   modifiedUser.uuid = uuid
@@ -36,13 +41,13 @@ usersRoute.put('/api/v1/users/:uuid', async (req, res) => {
 })
 
 // Deleta todos os usuários da lista
-usersRoute.delete('/api/v1/users', async (req, res) => {
+usersRoute.delete('/api/v1/users', async (req, res, next) => {
   await userRepository.removeAllUsers()
   res.send('All Users Removed')
 })
 
 // Deleta um usuário específico da lista
-usersRoute.delete('/api/v1/users/:uuid', async (req, res) => {
+usersRoute.delete('/api/v1/users/:uuid', async (req, res, next) => {
   const uuid = req.params.uuid
   await userRepository.removeUser(uuid)
   res.status(200).send('User Removed')
