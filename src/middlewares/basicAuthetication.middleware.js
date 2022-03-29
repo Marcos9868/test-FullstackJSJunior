5907f9526ac40ef7d7a74f401c20e1c635496257
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-03-29 17:37:16 -0300

    Verifica os headers da requisiçao e valida o jWT

---
+++ src/middlewares/basicAuthetication.middleware.js
@@ -0,0 +1,39 @@
+const ForbiddenError = require('../models/errors/forbidden.error')
+const userRepository = require('../repositories/user.repository')
+
+async function basicAuthetication(req, res, next) {
+  try {
+    const authorizationHeader = req.headers['authorization']
+
+    if (!authorizationHeader) {
+      throw new ForbiddenError('Uninformed Credentials')
+    }
+    const [autheticationType, token] = authorizationHeader.split(' ')
+
+    if (autheticationType !== 'Basic' || !token) {
+      throw new ForbiddenError('Invalid Authentication Type')
+    }
+
+    const tokenContent = Buffer.from(token, 'base64').toString('utf-8')
+
+    const [useremail, password] = tokenContent.split(':')
+    
+    if (!useremail || !password) {
+      throw new ForbiddenError('Unfilled credentials')
+    }
+
+    const user = await userRepository.findByUseremailAndPassword(useremail, password)
+
+    if (!user) {
+      throw new ForbiddenError('Invalid useremaiil or password')
+    }
+
+    req.user = user
+    next()
+
+  } catch (error) {
+    next(error)
+  }
+}
+
+module.exports = basicAuthetication
