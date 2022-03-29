4f5e1e1ceb13cef7b0bb0465c78d4dbfaff8f780
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-03-29 16:57:45 -0300

    Adiciona swaggerUI a API

--- src/index.js
+++ src/index.js
@@ -4,19 +4,27 @@ const api = express()
 const userRoute = require('./routes/users.route')
 const status = require('./routes/status.route')
 const errorHandler = require('./middlewares/error.handler.middleware')
+const authorizationRouter = require('./routes/authorization.route')
+const swaggerUI = require('swagger-ui-express')
+const swaggerDocs = require('./swagger.json')
 
 // Conexão com o banco de dados
 const db = require('./db/config')
 
-
 // Configuração da api para receber JSON
 api.use(express.json())
 
-// Status da API
+// Configuração do Swagger
+api.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
+
+// Rota Teste da API
 api.use(status)
 
 // Rota dos usuários
 api.use(userRoute)
+
+// Rota de autenticações
+api.use(authorizationRouter)
 
 // Configuração dos Handlers de Erro
 api.use(errorHandler)
