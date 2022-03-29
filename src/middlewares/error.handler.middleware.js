13318dbd850bc6e831c0d04aa2deeb1451b631cc
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-03-29 18:56:06 -0300

    Adiciona status Forbidden_Unauthorized no middleware

--- src/middlewares/error.handler.middleware.js
+++ src/middlewares/error.handler.middleware.js
@@ -1,10 +1,13 @@
 // Importa o modelo de erro que será apresentado no console
 const DatabaseError = require("../models/errors/database.error")
+const ForbiddenError = require('../models/errors/forbidden.error')
 
 // Função que o middleware irá executar
 const errorHandler = (error, req, res, next) => {
   if (error instanceof DatabaseError) {
     res.sendStatus(400) // BAD_REQUEST
+  } else if (error instanceof ForbiddenError) {
+    res.sendStatus(403) //FORBIDDEN_UNAUTHORIZED
   } else {
     res.sendStatus(500) // INTERNAL_SERVER_ERROR
   }
