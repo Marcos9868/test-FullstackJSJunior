2fba23ea94150deeb592d832c12bb17f4b063523
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-03-31 17:55:36 -0300

    Adiciona CORS na API

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 // Importações dos módulos necessários
 const express = require('express')
 const api = express()
+const cors = require('cors')
 const userRoute = require('./routes/users.route')
 const status = require('./routes/status.route')
 const errorHandler = require('./middlewares/error.handler.middleware')
 const authorizationRouter = require('./routes/authorization.route')
 const swaggerUI = require('swagger-ui-express')
-const swaggerDocs = require('./swagger.json')
+const swaggerDocs = require('./swagger-output.json')
 const jwtAuthenticationMiddleware = require('./middlewares/jwt.authentication.middleware')
 
 // Conexão com o banco de dados
 const db = require('./db/config')
+
+// Adiciona o CORS na API
+api.use(cors())
 
 // Configuração da api para receber JSON
 api.use(express.json())
 api.use(express.urlencoded({ extended: true }))
 
 // Configuração do Swagger
-api.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
+api.use('/api/v1/docs',cors(), swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
 // Rota Teste da API
 api.use(status)
@@ -30,6 +34,7 @@ api.use(jwtAuthenticationMiddleware)
 
 // Rota dos usuários
 api.use(userRoute)
+
 
 // Configuração dos Handlers de Erro
 api.use(errorHandler)
